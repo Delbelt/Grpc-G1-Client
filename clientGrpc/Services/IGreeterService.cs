43f20d7c2c1ca3a7c847fb02683451a86b54ba34
Nahuel Flores <nahuelhernanflores25@gmail.com>
2024-09-12 14:39:22 -0300

    REF: services and implementations

--- clientGrpc/Services/IGreeterService.cs
+++ clientGrpc/Services/IGreeterService.cs
@@ -1,15 +1,9 @@
-﻿using clientProto;
-
-namespace clientGrpc.Services
+﻿namespace clientGrpc.Services
 {
     public interface IGreeterService
     {
         // REFERENCIA: rpc SayHello(HelloRequest) returns(HelloReply) - Como esta representado en el archivo .proto
         // TRADUCCION: SayHello(string) returns string - Esta es la forma que debe adoptar el metodo
-        Task<string> SayHello(string message);
-        
-        // PRUEBA DE PERSISTENCIA: Se genera el objetoGrpc
-        Task<UserGrpc> GetUserGrpc(int id);
-        
+        Task<string> SayHello(string message);        
     }
 }
---
+++ clientGrpc/Services/Implementations/UserService.cs
@@ -0,0 +1,22 @@
+﻿using clientProto;
+
+namespace clientGrpc.Services.Implementations
+{
+    public class UserService : IUserService
+    {
+        private readonly UserGrpcService.UserGrpcServiceClient _UserGrpcService;
+        public UserService(UserGrpcService.UserGrpcServiceClient UserGrpcService)
+        {
+            _UserGrpcService = UserGrpcService;
+        }
+
+        public async Task<UserGrpc> GetUserGrpc(int id)
+        {
+            var fetchById = new RequestId { Id = id };
+
+            var response = await _UserGrpcService.GetUserGrpcAsync(fetchById);
+
+            return response;
+        }
+    }
+}
