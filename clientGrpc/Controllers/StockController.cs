d1cd990695183b3200901d170ebfc4d15e57eaca
homermm <monsalvohomero@gmail.com>
2024-09-20 22:24:23 -0300

    FEAT: StoreController added

---
+++ clientGrpc/Controllers/StockController.cs
@@ -0,0 +1,31 @@
+﻿using clientGrpc.Services;
+using Microsoft.AspNetCore.Mvc;
+
+using System.Threading.Tasks;
+
+namespace clientGrpc.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class StockController : ControllerBase
+    {
+        private readonly IStockService _stockService;
+
+        public StockController(IStockService stockService)
+        {
+            _stockService = stockService;
+        }
+
+        [HttpGet("{code}")]
+        public async Task<IActionResult> GetStockByCode(string code)
+        {
+            var stock = await _stockService.GetStockByCode(code);
+
+            if(stock == null)
+            {
+                return NotFound();
+            }
+            return Ok(stock);
+        }
+    }
+}
