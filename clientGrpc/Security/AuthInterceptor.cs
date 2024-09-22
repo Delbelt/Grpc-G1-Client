69027bf39c35673800adce5d184c5e1c1e12811e
Nahuel Flores <nahuelhernanflores25@gmail.com>
2024-09-22 16:05:37 -0300

    FEAT: security interceptor

---
+++ clientGrpc/Security/AuthInterceptor.cs
@@ -0,0 +1,41 @@
+﻿using Grpc.Core;
+using Grpc.Core.Interceptors;
+
+namespace clientGrpc.Security
+{
+    public class AuthInterceptor : Interceptor
+    {
+        private string _token;
+
+        public AuthInterceptor()
+        {
+            _token = string.Empty;
+        }
+
+        public void SetToken(string token)
+        {
+            _token = token;
+        }
+
+        public override AsyncUnaryCall<TResponse> AsyncUnaryCall<TRequest, TResponse>(
+        TRequest request,
+        ClientInterceptorContext<TRequest, TResponse> context,
+        AsyncUnaryCallContinuation<TRequest, TResponse> continuation)
+        {
+            var headers = new Metadata();
+
+            if (!string.IsNullOrEmpty(_token))
+            {
+                headers.Add("Authorization", $"Bearer {_token}");
+            }
+
+            context = new ClientInterceptorContext<TRequest, TResponse>(
+                context.Method,
+                context.Host,
+                context.Options.WithHeaders(headers)
+            );
+
+            return continuation(request, context);
+        }
+    }
+}
