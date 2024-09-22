aa8e5430d5cfc5fe16be3305479dee602cac0cbe
Nahuel Flores <nahuelhernanflores25@gmail.com>
2024-09-22 16:04:28 -0300

    FEAT: auth implementation

---
+++ clientGrpc/Services/Implementations/AuthService.cs
@@ -0,0 +1,19 @@
+﻿using authProto;
+using clientGrpc.Services;
+
+public class AuthService : IAuthService
+{
+    private readonly AuthGrpcService.AuthGrpcServiceClient _AuthGrpcService;
+    public AuthService(AuthGrpcService.AuthGrpcServiceClient authGrpcService)
+    {
+        _AuthGrpcService = authGrpcService;
+    }
+    public async Task<string> Login(string username, string password)
+    {
+        var loginRequest = new LoginRequest { Username = username, Password = password };
+
+        var response = await _AuthGrpcService.LoginGrpcAsync(loginRequest);
+
+        return response.Token;
+    }
+}
