79240de56255ab7f7524d509700bb8409f172c31
Nahuel Flores <nahuelhernanflores25@gmail.com>
2024-09-25 10:47:46 -0300

    FEAT: add Exception Handler

---
+++ clientGrpc/Handlers/GrpcExceptionHandler.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace clientGrpc.Handlers
+{
+    public static class GrpcExceptionHandler
+    {
+        public static IActionResult HandleGrpcException(Grpc.Core.RpcException ex, object responseDTO)
+        {
+            if (ex.StatusCode.Equals(Grpc.Core.StatusCode.Unauthenticated))
+            {
+                return new UnauthorizedObjectResult(responseDTO);
+            }
+
+            if (ex.StatusCode.Equals(Grpc.Core.StatusCode.Unavailable))
+            {           
+                return new ObjectResult(responseDTO)
+                {
+                    StatusCode = 500
+                };
+            }
+
+            return new NotFoundObjectResult(responseDTO);
+        }
+    }
+}
