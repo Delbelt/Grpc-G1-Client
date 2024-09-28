7eab4b29ecddc888e9ac1f5bae978798933a99c9
Euge16 <71157100+Euge16@users.noreply.github.com>
2024-09-28 14:36:26 -0300

    FEAT: Add new functionality -getStoresByState- in StoreController

--- clientGrpc/Controllers/StoreController.cs
+++ clientGrpc/Controllers/StoreController.cs
@@ -1,8 +1,11 @@
-﻿using clientGrpc.Services;
+﻿using clientGrpc.DTOs;
+using clientGrpc.Handlers;
+using clientGrpc.Services;
+using Grpc.Core;
 using Microsoft.AspNetCore.Mvc;
 
+using storeProto;
 
-using System.Threading.Tasks;
 
 namespace clientGrpc.Controllers
 {
@@ -11,21 +14,62 @@ namespace clientGrpc.Controllers
     public class StoreController : ControllerBase
     {
         private readonly IStoreService _storeService;
+        private readonly ILogger<IUserService> _logger;
 
-        public StoreController(IStoreService storeService)
+        public StoreController(IStoreService storeService, ILogger<IUserService> logger)
         {
             _storeService = storeService;
+            _logger = logger;
         }
 
         [HttpGet("{code}")]
         public async Task<IActionResult> GetStoreByCode(string code)
         {
-            var store = await _storeService.GetStoreByCode(code);
-            if (store == null)
+            try
             {
-                return NotFound();
+                var response = await _storeService.GetStoreByCode(code);
+                _logger.LogInformation("[StoreController][GetStoreByCode]: {message}", response.ToString());
+
+                var responseDTO = new mainDTO
+                {
+                    Content = response,
+                };
+
+                return Ok(responseDTO);
             }
-            return Ok(store);
+            catch (RpcException ex)
+            {
+                var responseDTO = new mainDTO { Content = ex.Status.Detail };
+
+                _logger.LogError("[StoreController][GetStoreByCode]: {error}", ex.Message);
+
+                return GrpcExceptionHandler.HandleGrpcException(ex, responseDTO);
+            }
+        }
+
+        [HttpGet("state/{active}")]
+        public async Task<IActionResult> GetStoresByState(bool active)
+        {
+            try
+            {
+                List<StoreGrpc> response = await _storeService.GetStoresByState(active);
+                _logger.LogInformation("[StoreController][GetStoresByState]: {message}", response.ToString());
+
+                var responseDTO = new mainDTO
+                {
+                    Content = response,
+                };
+
+                return Ok(responseDTO);
+            }
+            catch (RpcException ex)
+            {
+                var responseDTO = new mainDTO { Content = ex.Status.Detail };
+
+                _logger.LogError("[StoreController][GetStoresByState]: {error}", ex.Message);
+
+                return GrpcExceptionHandler.HandleGrpcException(ex, responseDTO);
+            }
         }
     }
 }
