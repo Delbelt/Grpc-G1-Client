0e0a64bd2c1fe47a4813936591c6864aa9e9dfee
Euge16 <71157100+Euge16@users.noreply.github.com>
2024-09-28 14:34:48 -0300

    FEAT: Add new functionality in StoreService

--- clientGrpc/Services/Implementations/StoreService.cs
+++ clientGrpc/Services/Implementations/StoreService.cs
@@ -14,9 +14,17 @@ namespace clientGrpc.Services.Implementations
         public async Task<StoreGrpc> GetStoreByCode(string code)
         {
             var request = new RequestCode { Code = code };
-            return await _client.getStoreGrpcAsync(request);
+            var response = await _client.getStoreGrpcAsync(request);
+            return response;
         }
 
+        public async Task<List<StoreGrpc>> GetStoresByState(bool active)
+        {
+            var request = new StoreStateRequest { Active = active };
+            var response = await _client.GetStoresByStateAsync(request);
+
+            return response.Stores.ToList();
+        }
 
     }
 }
