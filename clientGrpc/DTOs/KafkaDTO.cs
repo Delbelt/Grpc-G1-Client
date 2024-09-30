5296bd576abe3a639f7e6cdf6863f529d6828f3d
Nahuel Flores <nahuelhernanflores25@gmail.com>
2024-09-30 15:52:25 -0300

    FEAT: add kafka DTO message

---
+++ clientGrpc/DTOs/KafkaDTO.cs
@@ -0,0 +1,8 @@
+﻿namespace clientGrpc.DTOs
+{
+    public class KafkaDTO
+    {
+        public required Boolean Success { get; set; } 
+        public required string Message { get; set; }       
+    }
+}
