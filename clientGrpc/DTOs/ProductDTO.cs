bcf8b60415bfbc1f29fa1c41d4e565d9d2751e29
ErikaGeraldine <geraldinevaldez120@gmail.com>
2024-10-03 23:38:13 -0300

    feat: add proto DeleteProductGrpc

--- clientGrpc/DTOs/ProductDTO.cs
+++ clientGrpc/DTOs/ProductDTO.cs
@@ -3,10 +3,10 @@
     public class ProductDTO
     {
         public string Code { get; set; }
-        public string Name { get; set; }
-        public string Size { get; set; }
-        public byte[] Photo { get; set; }
-        public string Color { get; set; }
-        public bool Active { get; set; }
+        public string? Name { get; set; }
+        public string? Size { get; set; }
+        public byte[]? Photo { get; set; }
+        public string? Color { get; set; }
+        public bool? Active { get; set; }
     }
 }
