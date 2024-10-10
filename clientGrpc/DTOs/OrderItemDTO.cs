93a11930738dd440b91a3417651dad10de855c4f
Nahuel Flores <nahuelhernanflores25@gmail.com>
2024-10-09 21:30:39 -0300

    FEAT: kafka DTOS

---
+++ clientGrpc/DTOs/OrderItemDTO.cs
@@ -0,0 +1,10 @@
+﻿namespace clientGrpc.DTOs
+{
+    public class OrderItemDTO
+    {
+        public required string code { get; set; }
+        public required string color { get; set; }
+        public required string size { get; set; }
+        public required int quantity { get; set; } 
+    }
+}
---
+++ clientGrpc/DTOs/PurchaseOrderDTO.cs
@@ -0,0 +1,18 @@
+﻿using orderItemProto;
+
+namespace clientGrpc.DTOs
+{
+    public class PurchaseOrderDTO
+    {
+        public required int IdPurchaseOrder { get; set; }
+        public required string State { get; set; }
+        public required string Observations { get; set; }
+        public required DateTime RequestDate { get; set; }
+        public List<OrderItemDTO>? OrderItems { get; set; }
+    }
+    public class PostPurchaseOrderRequestDto
+    {
+        public required string CodeStore { get; set; }
+        public required List<OrderItemGrpc> Items { get; set; }
+    }
+}
